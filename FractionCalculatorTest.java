e41f2f4c423d423fa5e56bd3f6234faa215f324a
rasave01 <rasave01@mail.bbk.ac.uk>
2014-11-09 18:26:42 +0000

    Created FractionCalculatorTest and FractionCalculator classes

---
+++ FractionCalculatorTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package myCW2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author rasave01
+ *
+ */
+public class FractionCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

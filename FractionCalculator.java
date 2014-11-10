427a0187add629f3290bb47b69c8ce3e9cd120a0
rasave01 <rasave01@mail.bbk.ac.uk>
2014-11-10 20:34:16 +0000

    added StringTokaniser and changed FractionCalculator accordingly. Also added Quit functionality and test, tested OK

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -1,18 +1,22 @@
 package myCW2;
 
+import java.util.StringTokenizer;
+
 public class FractionCalculator {
 
 	public Fraction evaluate(Fraction frac, String inputString){
 		String operation = "";
 		Fraction result = frac;
-		//loop through the string
-		for (int i = 0; i < inputString.length(); i++){
-		    //current char
-			char current = inputString.charAt(i);        
-		    	if (current=='A' || current =='a') operation = "abs";
-		    	if (current=='N' || current =='n') operation = "neg";
-		    	if (current=='C' || current =='c') operation = "clear";
+		StringTokenizer st = new StringTokenizer(inputString, " "); 
+		while(st.hasMoreTokens()) { 
+			String next = st.nextToken(); 
+			if (next.equals("a")||next.equals("A")||next.equals("abs")){operation = "abs";}
+			if (next.equals("n")||next.equals("N")||next.equals("neg")){operation = "neg";}
+			if (next.equals("c")||next.equals("C")||next.equals("clear")){operation = "clear";}
+			if (next.equals("q")||next.equals("Q")||next.equals("quit")){operation = "quit";}
+
 		}
+		
 		if (operation.equals("abs")){
 			result = frac.absValue(); 
 	    }
@@ -22,7 +26,13 @@ public class FractionCalculator {
 		if (operation.equals("clear")){
 			result = new Fraction (0,1); 
 	    }
+		if (operation.equals("quit")){
+			System.out.println("The program has stopped");
+			result = new Fraction (0,1);
+			//throw here;  
+	    }
 		return result;
+	
 	}
-
+	
 }
--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -16,7 +16,7 @@ public class FractionCalculatorTest {
 	@Test
 	public void testEvaluateAbs() {
 		Fraction f = new Fraction(-2,15);
-		String str = "  a  ";
+		String str = " r 1/4 aa a  ";
 		FractionCalculator frCalc = new FractionCalculator();
 		Fraction actualOutput = frCalc.evaluate(f,str);
 		Fraction expectedOutput = new Fraction(2,15);
@@ -40,6 +40,16 @@ public class FractionCalculatorTest {
 		FractionCalculator frCalc = new FractionCalculator();
 		Fraction actualOutput = frCalc.evaluate(f,str);
 		Fraction expectedOutput = new Fraction(0,15);
+		assertEquals("Wrong answer!", expectedOutput, actualOutput);
+	}
+	
+	@Test
+	public void testEvaluateQuit() {
+		Fraction f = new Fraction(-1,3);
+		String str = "  Q  ";
+		FractionCalculator frCalc = new FractionCalculator();
+		Fraction actualOutput = frCalc.evaluate(f,str);
+		Fraction expectedOutput = new Fraction(0,2);
 		assertEquals("Wrong answer!", expectedOutput, actualOutput);
 	}
 }
